dcd4efffd2783c0b506fd74a4e56c2618c631c71
ayoade <auadesope@gmail.com>
2024-08-05 17:19:07 +0100

    updateddocker

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 // Middleware to parse JSON bodies
 app.use(express.json());
